447733054d7dc1f86895e30b1c66ff81f25c3d43
Jaweria-Tahir <jaweriatahir368@gmail.com>
2025-08-05 21:34:31 +0500

    adding practiceSet_01

---
+++ practiceSet_01.js
@@ -0,0 +1,42 @@
+//create variable of type string and try to add a number to it
+let str = "jiya";
+console.log(str + 1);
+
+
+//find datatype
+console.log(typeof (str + 1));
+
+
+//create const and try to change it and hold a number later
+const i = 
+{
+    name:"hamna",
+    section:"c",
+    class:14
+}
+//i = 54;
+//shows error
+
+
+//try to add col to object
+const a = 
+{
+    name:"hamna",
+    section:"c",
+    class:14
+}
+a["friend"] = "ali";
+a["name"] = "huma";
+console.log(a);
+
+//create a program of word meaning dictionary of five words
+const dict={
+appreciate:"celebrate",
+trigger:"stress",
+freedom:"liberty",
+ignore:"not think about",
+hardwork:"working too much"
+
+}
+
+console.log(dict.hardwork);
