12ad1c1a0712800733ba3b856fe6d34669151b18
Jaweria-Tahir <jaweriatahir368@gmail.com>
2025-08-05 17:20:36 +0500

    Add 02_datatypes.js

---
+++ 02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let name = "jiya"//string
+let age = 21//number
+let isLogged = false//boolean
+let state = null//null
+
+
+//number
+//bigint
+//string
+//boolean
+//null(stand alone value)
+//undefined(no value assigned)
+//symbol(unique)
+
+//object
+
+console.log(typeof "jiya");
+console.log(typeof age);
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
