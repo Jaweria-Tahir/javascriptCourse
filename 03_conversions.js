1d8f92a6e1a91e572063be362d61988546ab9897
Jaweria-Tahir <jaweriatahir368@gmail.com>
2025-08-05 18:07:20 +0500

    add conversions

---
+++ 03_conversions.js
@@ -0,0 +1,31 @@
+// let score =true;
+// let number = false;
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let value = Number(score);
+// let value2 = Number(number);
+
+// console.log(value);
+// console.log(value2);
+
+//notes
+//"33" into number
+//"33abc" into Nan
+//true into 1
+//false into 0
+
+// let isLoggedIn = "Jiya";
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+//notes
+//true is 1
+//empty string "" is false
+//string is true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
