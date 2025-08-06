34158d517a8fdb274f2c605e1e2176c4b6ea871f
Jaweria-Tahir <jaweriatahir368@gmail.com>
2025-08-06 13:57:22 +0500

    add 06

---
+++ 06_conditionalExpressions.js
@@ -0,0 +1,50 @@
+let a = 35;
+//if else
+if(a == 25)
+{
+    console.log("True");
+    
+}
+else {
+    console.log("False");
+    
+}
+
+//if else if
+if(a>0)
+{
+    console.log("False");
+    
+}
+else if(a==9)
+{
+    console.log("True");
+     
+}
+//switch
+let day = 3;
+switch(day)
+{
+    case 1:
+        console.log("Mon");
+        break;
+        
+    case 2:
+        console.log("Tues");
+        break;
+
+    case 3:
+        console.log("Wed");
+        break;
+
+    default:
+        console.log("Nothing");
+
+}
+
+//ternary operator
+let age = 4;
+
+let result = (age < 9) ? " True": "False";
+console.log(result);
+
---
+++ 07_loops.js
@@ -0,0 +1,22 @@
+//for loop
+for(let i = 1 ; i < 10 ; i++)
+{
+    console.log(i);
+    
+}
+//for in
+const obj = {
+    "jiya":10,
+    "ali":24,
+    "saad":26
+}
+for(let a in obj){
+    console.log("marks of " + a + "is: "+obj[a] );
+    
+}
+//for of
+for(let b of 'ali'){
+    console.log(b);
+}
+
+//while
