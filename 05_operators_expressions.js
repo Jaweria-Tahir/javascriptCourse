5baad53248440c24c30232308ac5d8842ece210e
Jaweria-Tahir <jaweriatahir368@gmail.com>
2025-08-06 13:25:55 +0500

    add 05_operators

---
+++ 05_operators_expressions.js
@@ -0,0 +1,29 @@
+//arithmetic operator
+let a = 45;
+let b = 4;
+console.log("a + b = ",a+b);
+console.log("a - b = ",a-b);
+console.log("a / b = ",a/b);
+console.log("a * b = ",a*b);
+console.log("a ** b = ",a**b);
+console.log("a % b = ",a%b);
+// console.log("a++ = ",a++);
+// console.log("a-- = ",a--);
+console.log("++a = ",++a);//46
+console.log("--a = ",--a);//45
+
+//comparison operators
+let c = 6;
+let d = "6";
+console.log(c == d);
+console.log(c != d);
+console.log(c === d);
+console.log(c !== d);
+console.log(c > d);
+
+//logical operators
+let x = 5;
+let y = 9;
+console.log(x<y && x==5);
+console.log(x<y || x==5);
+console.log(!x<y && x==5);
