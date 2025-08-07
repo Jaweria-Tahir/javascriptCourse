06dcbafbcd2dafb85523a76724df9994e87d0d7c
Jaweria-Tahir <jaweriatahir368@gmail.com>
2025-08-07 14:36:29 +0500

    adding strings,temperal literals and escape sequence characters

---
+++ 09_strings.js
@@ -0,0 +1,22 @@
+//double quotes
+let name = "Jiya";
+console.log(name);
+console.log(name[0]);
+console.log(name.length);
+//single quotes
+let friend = 'Hamna';
+console.log(friend);
+//template literals
+let boy1 = "Harry";
+let boy2 = "Haris";
+let sentence = `${boy1} is a friend of ${boy2}`;
+console.log(sentence);
+//Escape Sequence Characters
+let fruit = 'Bana\'na';//\'
+console.log(fruit);
+let fruit1 = 'Bana\nna';//n
+console.log(fruit1);
+let fruit2 = 'Bana\tna';//t
+console.log(fruit2);
+let fruit3 = 'Bana\rna';//\r
+console.log(fruit3);
