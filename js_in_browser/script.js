0d5fb020f5d40595a61b3515a5a4aa4b0e153130
Jaweria-Tahir <jaweriatahir368@gmail.com>
2025-08-13 16:48:48 +0500

    adding alert,prompt,confirm on browser

--- js_in_browser/script.js
+++ js_in_browser/script.js
@@ -34,15 +34,16 @@
 
 
 //alert,prompt,confirm
-alert("Enter value of a:");
-let a = prompt("Enter a:",9);
-a = Number.parseInt(a);
-alert("You entered a of type:"+typeof a);
-let write = confirm("Do you want to write it to the page");
-if(write)
-{
-    document.write(a);
-}
-else{
-    document.write("please allow me to write");
-}
+// alert("Enter value of a:");
+// let a = prompt("Enter a:",9);
+// a = Number.parseInt(a);
+// alert("You entered a of type:"+typeof a);
+// let write = confirm("Do you want to write it to the page");
+// if(write)
+// {
+//     document.write(a);
+// }
+// else{
+//     document.write("please allow me to write");
+// }
+
