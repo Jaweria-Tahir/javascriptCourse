9fecdd0d049eacf1ec90d349f2ee5002e94d3531
Jaweria-Tahir <jaweriatahir368@gmail.com>
2025-08-13 16:31:00 +0500

    adding alert, prompt,confirm

--- js_in_browser/script.js
+++ js_in_browser/script.js
@@ -6,27 +6,43 @@
 // document.write(a+b);
 
 
-console.log(console);//help in printing the entire console object
-console.log("Hi jaweria");//print msg
-console.error("This is an error");//print a message as error marking as red
-console.assert(5>53);//checking a condition
-console.assert(5<53);
-// console.clear();//clears the console
-const objt={"jiya":10,"ali":8,"saad":9};
-console.table(objt);
-console.warn("Hey dont cross red line");//shows warning in  browser console
-console.info("This is an important note");
-console.time("forLoop");//tells about the time taken to run the piece of code
-for (let i = 0; i < 5; i++) {
-    console.log(233);
+//console object
+// console.log(console);//help in printing the entire console object
+// console.log("Hi jaweria");//print msg
+// console.error("This is an error");//print a message as error marking as red
+// console.assert(5>53);//checking a condition
+// console.assert(5<53);
+// // console.clear();//clears the console
+// const objt={"jiya":10,"ali":8,"saad":9};
+// console.table(objt);
+// console.warn("Hey dont cross red line");//shows warning in  browser console
+// console.info("This is an important note");
+// console.time("forLoop");//tells about the time taken to run the piece of code
+// for (let i = 0; i < 5; i++) {
+//     console.log(233);
     
-}
-console.timeEnd("forLoop");
+// }
+// console.timeEnd("forLoop");
 
-console.time("whileLoop");//tells about the time taken to run the piece of code
-let i = 0;
-while(i<5) {
-    console.log(233);
-    i++;
+// console.time("whileLoop");//tells about the time taken to run the piece of code
+// let i = 0;
+// while(i<5) {
+//     console.log(233);
+//     i++;
+// }
+// console.timeEnd("whileLoop");
+
+
+//alert,prompt,confirm
+alert("Enter value of a:");
+let a = prompt("Enter a:",9);
+a = Number.parseInt(a);
+alert("You entered a of type:"+typeof a);
+let write = confirm("Do you want to write it to the page");
+if(write)
+{
+    document.write(a);
 }
-console.timeEnd("whileLoop");
+else{
+    document.write("please allow me to write");
+}
