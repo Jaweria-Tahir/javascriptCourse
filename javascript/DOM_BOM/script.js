941873590d70d57a5cf4188a3aafcee282bd3bf7
Jaweria-Tahir <jaweriatahir368@gmail.com>
2025-08-13 19:10:27 +0500

    DOM & BOM

---
+++ javascript/DOM_BOM/script.js
@@ -0,0 +1,11 @@
+//DOM(document object model)
+// window.console.log(window);
+// window.alert("hi");
+// console.log(document);
+// console.log(document.body);
+// dynamic changes
+// document.body.style.background = "pink";
+// document.body.style.color = "brown";
+//BOM
+alert("Harry");
+// location.href = "https://codewithharry.com";
