be72bf7be497a213f14452a47cc6f092ad8497e2
Jaweria-Tahir <jaweriatahir368@gmail.com>
2025-08-16 14:26:54 +0500

    adding walking the DOM

---
+++ walking-the-dom/script.js
@@ -0,0 +1,11 @@
+// all html elements can be accessed through javascript's object called document 
+//DOM has a tree like structure
+//when a web page is loaded browser automatically creates a DOM page
+
+console.dir(document);//access objet of document
+console.log(document);//accessing html body
+console.log(document.body.childNodes[1]);//accessing a child node
+console.log(document.body);
+console.log(document.head);
+console.log(document.documentElement);
+console.log(document.title);
